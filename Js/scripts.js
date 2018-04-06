56971c15e31d912b769c6fb230e60dc3766ff664
zamzammakoto <makotozamzam7@gmail.com>
2018-04-06 17:16:25 +0300

    added fuctions for play2 and play1

--- Js/scripts.js
+++ Js/scripts.js
@@ -6,3 +6,16 @@ function rollDice(){
 	die.innerHTML = randomNumber;
 }
  // end of function
+ //function for enabling and disabling the buttons
+ function enablePlayer2() {
+     document.getElementById("dice2").disabled = false;
+     document.getElementById("pass2").disabled = false;
+     document.getElementById("dice").disabled = true;
+     document.getElementById("pass").disabled = true;
+         }
+ function enablePlayer1() {
+     document.getElementById("dice").disabled = false;
+     document.getElementById("pass").disabled = false;
+     document.getElementById("dice2").disabled = true;
+     document.getElementById("pass2").disabled = true;
+ }
